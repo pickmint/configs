28cf7f49801b051ab7164662b8070c7b5216f6e4
Pickmint <116657828+pickmint@users.noreply.github.com>
2023-01-09 03:27:18 +0000

    🔧 Some formatting changes and files renamed Also, `os-setup.sh` had a big cleanup

--- os-setup.sh
+++ os-setup.sh
@@ -1,82 +1,99 @@
 #!bash
-case `which gsettings` in
-/*)
-    gsettings=(
-        "org.gnome.desktop.interface overlay-scrolling"
-    )
-    gsettings_key=(
-        "-bool false"
-    )
-    case "$1" in
-    install)
-        for i in "${!gesttings[@]}"; do
-	        gsettings set ${gesttings[$i]} ${gesttings_key[$i]}
-        done
-        ;;
-    uninstall)
-        for i in "${gesttings[@]}"; do
-            gsettings reset $i
-        done
-        ;;
-    esac
-    ;;
-esac
-case `uname` in
-Darwin)
+gsettings=(
+    "org.gnome.desktop.interface overlay-scrolling"
+    "org.gnome.nautilus.preferences default-folder-viewer"
+)
+gsettings_key=(
+    "-bool false"
+    "list-view"
+)
+macos_defaults=(
+    "NSGlobalDomain AppleShowAllExtensions"
+	"NSGlobalDomain AppleShowScrollBars"
+    "NSGlobalDomain AppleKeyboardUIMode"
+	"com.apple.desktopservices DSDontWriteNetworkStores"
+	"com.apple.dock showhidden"
+    "com.apple.finder AppleShowAllFiles"
+	"com.apple.finder FXPreferredViewStyle"
+	"com.apple.finder ShowPathbar"
+    "com.apple.finder QuitMenuItem"
+)
+macos_defaults_key=(
+    "-bool true"
+    "-string Always"
+    "-int 2"
+    "-bool true"
+    "-bool true"
+    "-bool true"
+    "-string clmv"
+    "-bool true"
+    "-bool true"
+)
+macos_killall=(
+    Dock
+    Finder
+)
+darwin_install(){
     case `which defaults` in
         /*)
-            defaults=(
-                "NSGlobalDomain AppleShowAllExtensions"
-	            "NSGlobalDomain AppleShowScrollBars"
-    	        "NSGlobalDomain AppleKeyboardUIMode"
-	            "com.apple.desktopservices DSDontWriteNetworkStores"
-	            "com.apple.dock showhidden"
-    	        "com.apple.finder AppleShowAllFiles"
-	            "com.apple.finder FXPreferredViewStyle"
-	            "com.apple.finder ShowPathbar"
-    	        "com.apple.finder QuitMenuItem"
-            )
-            defaults_key=(
-                "-bool true"
-	            "-string Always"
-    	        "-int 2"
-	            "-bool true"
-	            "-bool true"
-    	        "-bool true"
-	            "-string clmv"
-	            "-bool true"
-    	        "-bool true"
-            )
-            killall=(
-                Dock
-                Finder
-            )
-            case "$1" in
-            install)
-                osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/System/Library/Desktop Pictures/Solid Colors/Stone.png"'
-                for i in "${!defaults[@]}"; do
-	                defaults write ${defaults[$i]} ${defaults_key[$i]}
-                done
-                for i in "${killall[@]}"; do
-	                killall $i
-                done
-                ;;
-            uninstall)
-                for i in "${defaults[@]}"; do
-	                defaults delete $i
-                done
-                for i in "${killall[@]}"; do
-	                killall $i
-                done
+            osascript <<__EOF__
+                tell application "System Events"
+                    tell every desktop
+                        set picture to "/System/Library/Desktop Pictures/Solid Colors/Stone.png"
+                    end
+                end
+__EOF__
+            for i in "${!macos_defaults[@]}"; do
+                defaults write ${macos_defaults[$i]} ${macos_defaults_key[$i]}
+            done
+            for i in "${macos_killall[@]}"; do
+                killall $i
+            done
+        ;;
+    esac
+}
+case "$1" in
+    install)
+        case `uname` in
+        Darwin)
+            darwin_install
+        ;;
+        *)
+            case `which gsettings` in
+                /*)
+                    for i in "${!gesttings[@]}"; do
+	                    gsettings set ${gesttings[$i]} ${gesttings_key[$i]}
+                    done
                 ;;
             esac
-            ;;
-    esac
+        ;;
+        esac
     ;;
-esac
-case "$1" in
-    install);;
-    uninstall);;
+    uninstall)
+        case `uname` in
+            Darwin)
+                case `which defaults` in
+                    /*)
+                        for i in "${macos_defaults[@]}"; do
+	                        defaults delete $i
+                        done
+                        for i in "${macos_killall[@]}"; do
+	                        killall $i
+                        done
+                    ;;
+                esac
+            ;;
+            *)
+                case `which gsettings` in
+                    /*)
+                        for i in "${gesttings[@]}"; do
+                            gsettings reset $i
+                        done
+                    ;;
+                esac
+            ;;
+        esac
+    ;;
     *)
         cat<<__EOF__
 usage: $0 COMMAND
@@ -85,5 +102,5 @@ COMMANDS:
     install
     uninstall
 __EOF__
-        ;;
+    ;;
 esac
--- readme.md
+++ readme.md
@@ -2,20 +2,21 @@
 
 Personal User Configurations
 
-## Dependencies
-
-**Dependencies**: `GNU Make`, `GNU Stow`
-
-**Optional dependencies**: `Zsh`, `Bash`, `Git`, `Nano`, `Vim`, `Tmux`
 
 ## Installation
+
+    Dependencies: `GNU Make`, `GNU Stow`
 
 Before starting, please clone this repository to your
 local computer. `~/Projects/configs` is recommended.
 
-**Install**: `make install`
+**Install**: 
 
-**Uninstall**: `make uninstall`
+    make install
+
+**Uninstall**:
+
+    make uninstall
 
 ## Licence
 
